17a22acf959e015ab91dca2fb4bf958b03f63197
Eric dos Reis Santos <eric.santos@SHX0532.shxinfor.local>
2018-09-05 16:09:57 -0300

    Chapter 5 finished

--- client/app/controllers/NegotiationController.js
+++ client/app/controllers/NegotiationController.js
@@ -5,17 +5,30 @@ class NegotiationController {
     this._inputDate = $('#date');
     this._inputAmount = $('#amount');
     this._inputValue = $('#value');
+    this._negotiations = new Negotiations();
   }
 
   add(event) {
     event.preventDefault();
 
-    const date = DateConverter.toDate(this._inputDate.value);
-    const amount = parseInt(this._inputAmount.value);
-    const value = parseFloat(this._inputValue.value);
+    this._negotiations.add(this._createNegotiation());
 
-    const newNegotiation = new Negotiation(date, amount, value);
+    console.log(this._negotiations.getNegotiations());
+    this._clearForm();
+  }
 
-    console.log(DateConverter.toString(newNegotiation.date));
+  _createNegotiation() {
+    return new Negotiation(
+      DateConverter.toDate(this._inputDate.value),
+      parseInt(this._inputAmount.value),
+      parseFloat(this._inputValue.value),
+    );
+  }
+
+  _clearForm() {
+    this._inputDate.value = '';
+    this._inputAmount.value = 1;
+    this._inputValue.value = 0.0;
+    this._inputDate.focus();
   }
 }
